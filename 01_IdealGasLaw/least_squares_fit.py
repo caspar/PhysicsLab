0ef23c6519871ee3d8c51e9ee33fd70b9bdf87d1
Caspar <casparlant@gmail.com>
2015-09-29 18:15:12 -0400

    Ideal Gas Law - Caspar

---
+++ 01_IdealGasLaw/least_squares_fit.py
@@ -0,0 +1,34 @@
+# Lab 0
+# Linear Least Squares Fit
+# Author Caspar Lant
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load csv file
+DATA = "IdealGasLaw.csv";
+measurement, temperature, pressure, uncertainty, error = np.loadtxt(DATA, skiprows=5, unpack=True, delimiter=',');
+
+# plot temperature vs. pressure + error bars
+plt.xlabel("Temperature ($^\circ$C)");
+plt.ylabel("Pressure (lb/in$ ^2$)");
+plt.errorbar(temperature, pressure, error, linestyle = 'None', marker='o', mfc='orange', mec='r', ms=10, mew=1, ecolor = "k");
+
+# linear least squares fit line
+def least_squares_fit (x, y):
+    xavg = x.mean()
+    slope = (y*(x-xavg)).sum()/(x*(x-xavg)).sum()
+    intercept = y.mean()-slope*xavg
+    return slope, intercept
+
+slope, intercept = least_squares_fit(temperature, pressure);
+
+# create arrays to plot
+y1 = slope *  150 + intercept;  # y1 = m(x1) + b
+y2 = slope * -273 + intercept;  # y2 = m(x2) + b
+x_range = [-273.15, 150];          # array of x values
+y_range = [y2  , y1 ];          # array of y values
+
+# show the graph
+plt.plot(x_range,y_range);
+plt.show();
