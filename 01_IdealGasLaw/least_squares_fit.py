719aa68efa890967a85898ae8785d961af6e3dac
Caspar <casparlant@gmail.com>
2015-09-30 11:44:49 -0400

    Ideal Gas Law

--- 01_IdealGasLaw/least_squares_fit.py
+++ 01_IdealGasLaw/least_squares_fit.py
@@ -7,28 +7,31 @@ import matplotlib.pyplot as plt
 
 # load csv file
 DATA = "IdealGasLaw.csv";
-measurement, temperature, pressure, uncertainty, error = np.loadtxt(DATA, skiprows=5, unpack=True, delimiter=',');
+measurement, pressure, temperature, uncertainty, error = np.loadtxt(DATA, skiprows=5, unpack=True, delimiter=',');
 
 # plot temperature vs. pressure + error bars
-plt.xlabel("Temperature ($^\circ$C)");
-plt.ylabel("Pressure (lb/in$ ^2$)");
-plt.errorbar(temperature, pressure, error, linestyle = 'None', marker='o', mfc='orange', mec='r', ms=10, mew=1, ecolor = "k");
+plt.xlabel("Pressure (kPa)");
+plt.ylabel("Temperature (K)");
+plt.title("Temperature vs. Pressure");
+plt.errorbar(pressure, temperature, error, uncertainty, linestyle = ':', mec='r', ms=0, ecolor = "red");
 
 # linear least squares fit line
 def least_squares_fit (x, y):
     xavg = x.mean()
-    slope = (y*(x-xavg)).sum()/(x*(x-xavg)).sum()
+    slope = ( y * ( x - xavg)).sum() / (x*(x-xavg)).sum()
     intercept = y.mean()-slope*xavg
     return slope, intercept
 
 slope, intercept = least_squares_fit(temperature, pressure);
 
 # create arrays to plot
-y1 = slope *  150 + intercept;  # y1 = m(x1) + b
-y2 = slope * -273 + intercept;  # y2 = m(x2) + b
-x_range = [-273.15, 150];          # array of x values
-y_range = [y2  , y1 ];          # array of y values
+y1 = slope *  400 + intercept;  # y1 = m(x1) + b
+y2 = slope *  0   + intercept;   # y2 = m(x2) + b
+x_range = [0, 400];             # array of x values
+y_range = [y2, y1];             # array of y values
+
+print("slope: %d intercept: %d", slope, intercept)
 
 # show the graph
-plt.plot(x_range,y_range);
+plt.plot(y_range, x_range, color="blue");
 plt.show();
