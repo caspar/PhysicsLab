d8dd3f53b29734d7093105292bcfa5308f6ed276
Caspar <casparlant@gmail.com>
2015-12-16 17:57:03 -0500

    Final

--- final/OscillationsOnAString.py
+++ final/OscillationsOnAString.py
@@ -1,22 +1,40 @@
 import numpy as np;
 import matplotlib.pyplot as plot
 import math;
+from scipy.optimize import curve_fit
+
+#http://www.physics.nyu.edu/pine/pymanual/html/chap8/chap8_fitting.html
 
 data = "Amaxdata.csv";
 
 # load csv
-time, displacement, error = np.loadtxt(data, skiprows=0, unpack = True, delimiter=',');
+t_max, A_max, A_error = np.loadtxt(data, skiprows=0, unpack = True, delimiter=',');
+# plot.errorbar(t_max, A_max, A_error);
+
 times = [];
-logs = []
+# logs = np.array(logs, dtype=float);
 
-for t in time:
-    print time[t+1] - time[t]
+def exp(x,a,b,c):
+    return a * np.exp(-b * x) + c;
 
-for d in displacement:
-    print math.log(displacement[d]);
+def gamma(A, t):
+    # ln(A_max(t_max)) = ln(A_0) - .5 * gamma * t_max => gamma =
+    gamma = (math.log(A_max[0]) - math.log(A))*2/t
+    print gamma
+    return gamma
 
-print (time[len(time)-1] - time[0])/(len(time)-1)
-# plot.plot(time, displacement);
-plot.errorbar(time, displacement, error);
-# plot.plot(time, logs);
+gamma = np.array(A_max, dtype=float);
+
+# for t in t_max:
+#     print t_max[t+1] - t_max[t]
+
+# for d in A_max:
+#     logs[d] = math.log(A_max[d]);
+
+# print (t_max[len(t_max)-1] - t_max[0])/(len(t_max)-1)
+# plot.plot(t_max, A_max);
+# plot.plot(t_max, np.polyfit(A_max));
+for i in t_max:
+    plot.plot(t_max[i], gamma(A_max[i], t_max[i]))
+# plot.plot(t_max, logs);
 plot.show();
