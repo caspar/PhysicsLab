562337953f51be32dcd1a7d1ffe0ea4d3e649eb6
Caspar <casparlant@gmail.com>
2016-03-03 18:14:07 -0500

    Labstuffs

--- 14_Seismometer/data/ReadDataFitPlot.py
+++ 14_Seismometer/data/ReadDataFitPlot.py
@@ -4,37 +4,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
-import os
-
-FILENAME = "blow2_5.txt"
-TIMESTAMP = datetime.now().strftime('%H:%M:%S')
-OUTPUT = ''+ os.path.splitext(FILENAME)[0] + '_' + TIMESTAMP + '.png'
-print OUTPUT
-
 # Function defining exponentially decaying sinusoidal
 def thetaU(t, A, w0, Q, phi):
     wu = w0 * np.sqrt(1.0-1.0/(4.0*Q*Q))
     return A * np.exp(-0.5*w0*t/Q) * np.sin(wu*t + phi)
 # User sets these parameters to best match the data
-A = 0.3
-T = 14.9
-Q = 8.5
+￼￼A = 0.12
+T = 17.0
+Q = 10.0
 # Amplitude
 # Period (w0 = 2 pi / T)
 # Q = quality factor (determines damping)
-phase = 85                 # phase in degrees
+phase = 90.                 # phase in degrees
 # Convert phase in degrees to phase in radius for use in function
 phi = np.pi * phase / 180.0     # phase in radians
-time, theta = np.loadtxt(FILENAME, skiprows=130 , unpack = True)
+time, theta = np.loadtxt(’FreeDecay2.txt’, skiprows=250 , unpack=True)
 fit = thetaU(time, A, 2.0*np.pi/T, Q, phi)
-plt.plot(time, fit, color='black')
-plt.axhline(color="gray")
+plt.plot(time, fit)
+plt.axhline(color=’gray’)
 plt.plot(time, theta)
-plt.annotate("$A = 0.25$ \n $T = 14.25$ \n $Q = 9$ \n $\phi = 40^{\circ}$", (95,0.1))
 plt.show()
-
-fig = plt.figure()
-fig.savefig(OUTPUT)
-# format='png',
-        # transparent=True, bbox_inches=None, pad_inches=0.1,
-        # frameon=None)
--- 14_Seismometer/data/allplot.py
+++ 14_Seismometer/data/allplot.py
@@ -1,31 +1,10 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import datetime
 
-filename = "edit_blow5.txt"
-time1, theta1 = np.loadtxt("edit_blow2_5.txt", skiprows=2 , unpack=True)
-time2, theta2 = np.loadtxt(filename, skiprows=2 , unpack=True)
-time3, theta3 = np.loadtxt("blow8_5.txt", skiprows=2, unpack=True)
+time, theta = np.loadtxt("blow2_5.txt", skiprows=250 , unpack=True)
+time, theta = np.loadtxt("blow5.txt", skiprows=250 , unpack=True)
 # time, theta = np.loadtxt("blow", skiprows=250 , unpack=True)
-
-timestamp = datetime.now().strftime('%H:%M:%S')
-print timestamp
-
 plt.axhline(color="gray")
-plt.plot(time1, theta1)
-plt.plot(time2+.8, theta2)
-plt.plot(time3+2.0, theta3)
-plt.xlabel("Time (s)")
-plt.ylabel("Voltage (V)")
-plt.annotate("2.5cm", xy=(31.45,0.127), xytext=(29.45, 0.2),arrowprops=dict(arrowstyle="->", color='green'), color='green')
-plt.annotate("5cm", xy=(45.92,0.079), xytext=(45, 0.15),arrowprops=dict(arrowstyle="->", color='blue'), color='blue')
-plt.annotate("8.5cm; \nOverdamped", xy=(10,0.035), xytext=(6, 0.25),arrowprops=dict(arrowstyle="->", color='red'), color='red')
+plt.plot(time, theta)
 plt.show()
-#
-# savefig(fname, dpi=None, facecolor='w', edgecolor='w',
-#         orientation='portrait', papertype=None, format=None,
-#         transparent=False, bbox_inches=None, pad_inches=0.1,
-#         frameon=None)
-
-# plt.close("all")
