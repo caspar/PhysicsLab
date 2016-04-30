aa794c408263fe119bec4fc99e633dfbcc27f1da
Caspar <casparlant@gmail.com>
2016-04-30 15:09:43 -0400

    20_NuclearSpectroscopy/

--- 20_NuclearSpectroscopy/data/plot.py
+++ 20_NuclearSpectroscopy/data/plot.py
@@ -6,18 +6,38 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import os
 
-FILENAME = "-7.55 gain .6keV Covered.txt"
-TIMESTAMP = datetime.now().strftime('%D-%H:%M:%S')
-OUTPUT = ''+ os.path.splitext(FILENAME)[0] + '_' + TIMESTAMP + '.png'
-print OUTPUT
+# Lead 1.1 9,246
+#
+# Lead 2.4 7,862
+#
+# Lead 3.4 6,849
+#
+# Lead 8.4 4980
+#
+# 0 10,338
 
-time, events, channel, energy = np.loadtxt(FILENAME, skiprows=6 , unpack = True)
-plt.axhline(color="gray")
-plt.plot(time, events)
+counts = np.array([10338, 9246, 7862, 6849, 4980])
+thickness = np.array([0, 1.1, 2.4, 3.4, 8.4])
+
+def least_squares_fit (x, y):
+    xavg = x.mean()
+    slope = ( y * ( x - xavg)).sum() / (x*(x-xavg)).sum()
+    intercept = y.mean()-slope*xavg
+    return slope, intercept
+
+slope, intercept = least_squares_fit(thickness, np.log(counts));
+
+y1 = slope *  10  + intercept;  # y1 = m(x1) + b
+y2 = slope *  0   + intercept;   # y2 = m(x2) + b
+x_range = [0, 10];             # array of x values
+y_range = [y2, y1];             # array of y values
+
+plt.xlabel("Lead Thickness (mm)")
+plt.ylabel(r'ln(Counts) ($\beta$)')
+plt.title('Beta Particles Detected vs. Shield Thickness')
+
+plt.plot(thickness, np.log(counts), 'o')
+plt.plot(x_range, y_range, '-', color="blue");
+print(slope, intercept)
+# plt.axis([0, 10, 8, 10])
 plt.show()
-
-fig = plt.figure()
-fig.savefig(OUTPUT)
-# format='png',
-        # transparent=True, bbox_inches=None, pad_inches=0.1,
-        # frameon=None)
